09e6eb777d0a2f3d04ee4054a5ad3d69e364a508
Felipe Iraola <felipe.iraola@gmail.com>
2025-11-01 23:20:39 -0300

    Tipos de variable

--- clase 1 - Principios.R
+++ clase 1 - Principios.R
@@ -1,2 +1,34 @@
 #Tipos de variables
 
+#integer
+
+x <- 2L
+typeof(x)
+
+#DOUBLE - DECIMALES
+
+y<- 2.5
+typeof(y)  
+
+#complejos
+
+z <- 3+2i
+typeof(z)
+
+#character
+
+a <- "a"
+
+typeof(a)
+
+
+# logical
+
+q1<- TRUE
+q2<- FALSE
+
+typeof(q1)
+typeof(q2)
+
+
+
