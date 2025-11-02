af16fd495b6b8511366af711209535057b89e62a
Felipe Iraola <felipe.iraola@gmail.com>
2025-11-02 00:32:16 -0300

    If statemtnt

--- clase 1 - for.R
+++ clase 1 - for.R
@@ -1,1 +1,14 @@
-S\
+
+
+# ----------- -2 --------- -1 --------- 0 ------- 1 ----- 2
+
+rm(answer)
+x<-rnorm(1)
+
+if( x>1 ){
+    answer<"Es mas grande que uno"
+}else if (x>= -1){
+   answer<-"Entre -1 y 1"
+}else{
+   answer<-"Mas chico que -1"
+  }
